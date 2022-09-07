4109a3af92bac3b4ff5523c3dc95c0a30ebaee09
Babkina Ekaterina <EBabkina@alfabank.ru>
2022-09-08 00:17:29 +0300

    Homework task 3

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testHello(){
+        String name = "Katya";
+        System.out.println("Hello from "+ name);
+    }
+}
